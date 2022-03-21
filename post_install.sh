354cbd03e63d511563b84e66958b2efbd1cd75ff
Richard <richard2891@gmail.com>
2022-03-21 18:25:57 -0400

    update application name in the rc.xml file and novnc index.html file.

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -x
 IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
 
+APP_NAME="HandBrake"
+
+sed -i.bak "s/<application type=\"normal\">/<application type=\"normal\" title=\"${APP_NAME}\">/" /usr/local/etc/xdg/openbox/rc.xml
+sed -i.bak "/<application type=\"normal\" title=\"${APP_NAME}\">/a \\
+    <layer>below</layer>
+" /usr/local/etc/xdg/openbox/rc.xml
+
 ln -s /usr/local/bin/python3.8 /usr/local/bin/python
 
-ln -s /usr/local/libexec/novnc/vnc_lite.html /usr/local/libexec/novnc/index.html
+cp /usr/local/libexec/novnc/vnc_lite.html /usr/local/libexec/novnc/index.html
+
+sed -i.bak "s|<title>noVNC</title>|<title>${APP_NAME}</title>|" /usr/local/libexec/novnc/index.html
 
 openssl req -new -x509 -days 365 -nodes -out self.pem -keyout /usr/local/libexec/novnc/utils/websockify/self.pem -subj "/C=CA/ST=ONTARIO/L=TORONTO/O=Global Security/OU=IT Department/CN=vnc"
 
